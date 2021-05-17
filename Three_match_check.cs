67de18d6508ee06b27dce7bf4b7630aa83385d36
ironee1617 <ironee1617@gmail.com>
2021-05-17 23:16:50 +0900

    5.17

--- Three_match_check.cs
+++ Three_match_check.cs
@@ -19,14 +19,24 @@ public class Three_match_check : MonoBehaviour
         Right,
         Left
     }
+    // up down left right
+    Pair<int, int>[] Direction;
 
     List<Fruit> destroy_fruit = new List<Fruit>();
 
     void Start()
     {
-        while(checked_fruit.Count < 4)
+        Direction = new Pair<int, int>[4];
+        Direction[0] = new Pair<int, int>(-1, 0);
+        Direction[1] = new Pair<int, int>(1, 0);
+        Direction[2] = new Pair<int, int>(0, -1);
+        Direction[3] = new Pair<int, int>(0, 1);
+
+        while (checked_fruit.Count < 4)
             checked_fruit.Add(new List<Fruit>());
 
+        check += AddClickFruit;
+        //check += FruitCheck;
         check += UpCheck;
         check += DownCheck;
         check += RightCheck;
@@ -34,6 +44,29 @@ public class Three_match_check : MonoBehaviour
 
         end_check += MatchCheck;
     }
+
+    public static void ChainFunc()
+    {
+        
+    }
+
+    private void AddClickFruit(Fruit fruit)
+    {
+        destroy_fruit.Add(fruit);
+    }
+    //private void FruitCheck(Fruit fruit)
+    //{
+    //    for(int dir = 0; dir < 4; dir++)
+    //    {
+    //        Pair<int, int> local = Direction[dir];
+    //        Fruit check_fruit = Three_match_rule.FruitLayout[fruit.local.First + local.First, fruit.local.Second + local.Second];
+    //        if (fruit.fruit_Color == check_fruit.fruit_Color)
+    //        {
+    //            EqualColor(checked_fruit, dir, check_fruit, FruitCheck);
+    //        }
+    //    }
+
+    //}
 
     private void UpCheck(Fruit fruit)
     {
@@ -43,6 +76,7 @@ public class Three_match_check : MonoBehaviour
             EqualColor(checked_fruit, (int)DIRECTION.Up, up_fruit, UpCheck);
         }
     }
+
     private void DownCheck(Fruit fruit)
     {
         Fruit down_fruit = Three_match_rule.FruitLayout[fruit.local.First + 1, fruit.local.Second];
@@ -51,6 +85,7 @@ public class Three_match_check : MonoBehaviour
             EqualColor(checked_fruit, (int)DIRECTION.Down, down_fruit, DownCheck);
         }
     }
+
     private void RightCheck(Fruit fruit)
     {
         Fruit right_fruit = Three_match_rule.FruitLayout[fruit.local.First, fruit.local.Second + 1];
@@ -59,13 +94,13 @@ public class Three_match_check : MonoBehaviour
             EqualColor(checked_fruit, (int)DIRECTION.Right, right_fruit, RightCheck);
         }
     }
+
     private void LeftCheck(Fruit fruit)
     {
         Fruit left_fruit = Three_match_rule.FruitLayout[fruit.local.First, fruit.local.Second - 1];
         if (fruit.fruit_Color == left_fruit.fruit_Color)
         {
             EqualColor(checked_fruit, (int)DIRECTION.Left, left_fruit, LeftCheck);
-            
         }
     }
 
@@ -76,8 +111,6 @@ public class Three_match_check : MonoBehaviour
             check_func(fruit);
         else
             return;
-
-        
     }
 
     private void ListInit()
@@ -93,7 +126,6 @@ public class Three_match_check : MonoBehaviour
 
     private void MatchCheck()
     {
-        //bool match_success = false;
         List<int> match_line = new List<int>();
 
         ThreeMatchCheck(match_line);
@@ -102,21 +134,19 @@ public class Three_match_check : MonoBehaviour
             FourMatchCheck(match_line);
             DestroyFruit(ListInit);
         }
+
+        destroy_fruit.Clear();
     }
 
     private void DestroyFruit(Action Callback)
     {
-        Debug.Log(destroy_fruit.Count);
         for (int i = destroy_fruit.Count - 1; i >= 0; i--)
         {
             destroy_fruit[i].gameObject.SetActive(false);
-            destroy_fruit.RemoveAt(i);
         }
 
         Callback();
     }
-
-
 
     private void ThreeMatchCheck(List<int> list)
     {
@@ -126,6 +156,10 @@ public class Three_match_check : MonoBehaviour
             {
                 destroy_fruit.AddRange(checked_fruit[i]);
                 list.Add(i);
+            }
+            else if (checked_fruit[i].Count.Equals(1))
+            {
+                //if(checked_fruit[i])
             }
         }
     }
@@ -143,23 +177,4 @@ public class Three_match_check : MonoBehaviour
             }
         }
     }
-
-    private bool FiveMatchCheck()
-    {
-        return false;
-        //if (checked_fruit[i])
-    }
-
-    //void Check_Fruit(Fruit fruit, DIRECTION direction)
-    //{
-    //    Fruit fruit = Three_match_rule.FruitLayout[fruit.local.First - 1, fruit.local.Second];
-    //    if (fruit.fruit_Color == left_fruit.fruit_Color)
-    //    {
-    //        checked_fruit[(int)DIRECTION.Left].Add(left_fruit);
-    //        if (checked_fruit[(int)DIRECTION.Left].Count < 2)
-    //            UpCheck(left_fruit);
-    //        else
-    //            return;
-    //    }
-    //}
 }
